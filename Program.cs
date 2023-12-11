510991884c7a7629539ef0f9549c3eb75b4b6d65
Mateus Melo <mateusdevmelo@gmail.com>
2023-12-10 23:33:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Heranca.Models;
+
+Aluno a1 = new Aluno();
+a1.Nome = "Mateus";
+a1.Idade = 29;
+a1.Nota = 10;
+a1.apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Leonardo";
+p1.Idade = 30;
+p1.Salario = 5000;
+p1.apresentar();
